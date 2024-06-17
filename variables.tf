44a414a1d88536d298ad2e2b671fa9e55bcdde85
ArunkumarThumati <arundevops100@gmail.com>
2024-06-17 16:31:48 +0530

    terraform dev

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project_name" {
+  type = string
+}
+
+variable "Environment" {
+  type = string
+  default = "dev"
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "sg_description" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
